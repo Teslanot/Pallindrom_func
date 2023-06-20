8db3f82a219e97edfc99871921b20e16d6a3d064
Teslanot <136720777+Teslanot@users.noreply.github.com>
2023-06-20 11:25:57 +0300

    Add files via upload

---
+++ palindrom.py
@@ -0,0 +1,16 @@
+
+
+
+def pali_func(word):
+    if word == word[::-1]:
+        print(True, word)
+    else:
+        print(False, word)
+
+
+
+
+
+pali_func('шалаш')
+pali_func('петя')
+pali_func('lesel')
